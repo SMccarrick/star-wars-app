43045b1325f7e078910fae9735e934aeeff9cb98
housepartystevee <smccarrick2804@gmail.com>
2019-02-28 11:12:18 +1300

    Added progress for card components

---
+++ src/components/CardList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ people }) => {
+	console.log(people.results);
+	return (
+		<div>
+			{people.map(results => (
+				<Card key={results.height} name={results.name} />
+			))}
+		</div>
+	);
+};
+
+export default CardList;
