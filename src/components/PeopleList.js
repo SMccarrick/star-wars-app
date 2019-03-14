7d75449ee51d0b3dcde2f87d2b961aac13319bcd
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-14 15:21:29 +1300

    Decalared state of people to an empty array.

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -2,5 +2,11 @@ import React, { Component } from "react";
 import Card from "./Card";
 
 class PeopleList extends Component {
+  // Set default state of movies to a blank array
+  // If the data doesnt come in its just a blank array
+  state = {
+    people: []
+  };
+
 
 export default PeopleList;
