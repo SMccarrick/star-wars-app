6eabfaa4d7f96b2e81867a225719dd413f764386
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-14 15:30:29 +1300

    Fixed TODO comments

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import Header from "./components/Header";
 import "./App.css";
 
 
-  // TODO(1): Fix state and get the data to pass down through to the cardList Component
-  // TODO(2): Work out a way to have mutiple lists using only one list component
-  // TODO(3): Link other categories to list
-  // TODO(4): Add Headings for each category
-  // TODO(5): Add Scroll component to lists
-  // TODO(6): Search filter input component(smart component)...
-  // ADD BELOW: <CardList people />
+// TODO(1): Fix state and get the data to pass down through to the cardList Component
+// TODO(2): Work out a way to have mutiple lists using only one list component
+// TODO(3): Link other categories to list
+// TODO(4): Add Headings for each category
+// TODO(5): Add Scroll component to lists
+// TODO(6): Search filter input component(smart component)...
+// ADD BELOW: <CardList people />
 
 export default App;
