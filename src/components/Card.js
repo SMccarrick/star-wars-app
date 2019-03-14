78edfcfc88a859c631d5459ed865fcca6a05bf5b
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-14 14:09:37 +1300

    Removed tachyon properties

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Card = ({ name }) => {
-	return (
-		<div className="tc bg-yellow dib br3 pa3 ma2 grow bw2 shadow=5">
-			<div>
-				<h2>{name}</h2>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div>
+        <h2>{name}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
