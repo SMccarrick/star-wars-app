77cffac67c6632c5b72217a82acb0fb5d2dc5ae2
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-14 15:24:12 +1300

    Declared state of planets to empty array.

--- src/components/PlanetList.js
+++ src/components/PlanetList.js
@@ -2,5 +2,11 @@ import React, { Component } from "react";
 import Card from "./Card";
 
 class PlanetList extends Component {
+  // Set default state of movies to a blank array
+  // If the data doesnt come in its just a blank array
+  state = {
+    planets: []
+  };
+
 
 export default PlanetList;
