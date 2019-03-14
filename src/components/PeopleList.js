f08b24c2219bbf117e0ce07e12df3e4793d6f165
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-14 15:22:37 +1300

    Used componentDidMount to create asynchronous fetch request and set its state to people.

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -8,5 +8,19 @@ class PeopleList extends Component {
     people: []
   };
 
+  // Fetch data from the API
+  async componentDidMount() {
+    try {
+      const res = await fetch("https://swapi.co/api/people/");
+      const people = await res.json();
+
+      this.setState({
+        people: people.results
+      });
+    } catch (e) {
+      console.log("error", e);
+    }
+  }
+
 
 export default PeopleList;
