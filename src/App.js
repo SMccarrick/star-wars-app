6fda6b3eebd594f8fc09a69cefac3ab49c307308
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-14 15:27:39 +1300

    Removed class component as it was obsolete in this instance.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,6 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    // Declare empty arrays for data from each url
-    this.state = {
-      people: [],
-      planets: [],
-      films: []
-    };
-  }
 
   // Fetches data from the Star Wars API
   async componentDidMount() {
