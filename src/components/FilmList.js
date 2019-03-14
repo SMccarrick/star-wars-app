f7adace16eec69c8afb653f806111ccdca796181
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-14 15:18:45 +1300

    Used componentDidMount to create asynchronous fetch request and set its state to films.

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -3,4 +3,19 @@ import Card from "./Card";
 
 class FilmList extends Component {
 
+  // Fetch data from the API
+  async componentDidMount() {
+    try {
+      const res = await fetch("https://swapi.co/api/films/");
+      const films = await res.json();
+
+      this.setState({
+        films: films.results
+      });
+    } catch (e) {
+      console.log("error", e);
+    }
+  }
+
+
 export default FilmList;
