6dae84b3532e333525f3d8d3add36c1491241ffa
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-18 16:44:47 +1300

    Added a styled component for the film styles

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
 const Film = ({ title }) => {
   return (
     <CardStyles>
@@ -13,4 +15,12 @@ export default Film;
 Film.propTypes = {
   title: PropTypes.string.isRequired
 };
+
+const CardStyles = styled.div`
+  text-align: center;
+  margin: 1rem;
+  padding: 1rem;
+  background: #bdbdbd;
+  border-radius: 25px;
+`;
 
