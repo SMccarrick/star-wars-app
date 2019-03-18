947455a8bee2ed6c3f7060d59402a5fb68b799e8
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-18 16:45:37 +1300

    Changed main heading

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1>STAR NEWS</h1>
-      <hr />
-      <h2>A news round up from a galaxy far far away.</h2>
+      <MainHeading>Star Wars</MainHeading>
     </div>
   );
 };
