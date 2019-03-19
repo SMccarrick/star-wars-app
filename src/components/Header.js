5cad2edb7118a84052651b6978347babc27ed273
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-19 20:19:40 +1300

    Added header route to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = () => {
   return (
-    <div>
+    <Link to="/">
       <MainHeading>Star Wars</MainHeading>
-    </div>
+    </Link>
   );
 };
 
