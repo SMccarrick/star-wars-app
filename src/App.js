45f8dc5ab80ac932456c1fbfedd9cd2dc5fb129b
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-22 15:57:32 +1300

    Removed old comments

--- src/App.js
+++ src/App.js
@@ -22,13 +22,5 @@ const App = () => {
     </Router>
   );
 };
-// <Route path="/:id" component={MovieDetail} />
-// TODO(1): Fix state and get the data to pass down through to the cardList Component
-// TODO(2): Work out a way to have mutiple lists using only one list component
-// TODO(3): Link other categories to list
-// TODO(4): Add Headings for each category
-// TODO(5): Add Scroll component to lists
-// TODO(6): Search filter input component(smart component)...
-// ADD BELOW: <CardList people />
 
 export default App;
