13ffe78dc4a8b3177dcc7c4268edd63f4485a0a9
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-29 18:30:58 +1300

    Updated rulings to improve codebase.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,49 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
+    node: true,
   },
-  extends: ["airbnb", "prettier", "prettier/react"],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
     window: true,
     document: true,
     localStorage: true,
     FormData: true,
     FileReader: true,
     Blob: true,
-    navigator: true
+    navigator: true,
   },
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  plugins: ["prettier", "react"],
+  plugins: ['prettier', 'react', 'import', 'jsx-a11y'],
   rules: {
-    "react/jsx-filename-extension": [
+    'arrow-parens': 'off',
+    'object-curly-newline': 'off',
+    'no-mixed-operators': 'off',
+    'function-paren-newline': 'off',
+    'space-before-function-paren': 0,
+    'no-plusplus': 'off',
+    'react/prop-types': 0,
+    'no-underscore-dangle': 0,
+    'import/imports-first': ['error', 'absolute-first'],
+    'import/newline-after-import': 'error',
+    'no-param-reassign': 0,
+    'prettier/prettier': ['error'],
+    camelcase: [1, { properties: 'never' }],
+    'react/jsx-filename-extension': [
       1,
       {
-        extensions: [".js", ".jsx"]
-      }
+        extensions: ['.js', '.jsx'],
+      },
     ],
-    camelcase: [1, { properties: "never" }],
-    "react/prop-types": 0,
-    "no-underscore-dangle": 0,
-    "import/imports-first": ["error", "absolute-first"],
-    "import/newline-after-import": "error",
-    "no-param-reassign": 0
-  }
+  },
 };
