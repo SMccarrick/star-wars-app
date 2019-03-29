c844507be4aa61331340dedcbd578901da6dd22a
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-29 19:18:31 +1300

    Added fetchCharacter component class

---
+++ src/components/FetchCharacter.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+// Fetch data from the API
+/* async componentDidMount() {
+  try {
+    const res = await fetch('https://swapi.co/api/people/');
+    const people = await res.json();
+
+    this.setState({
+      characters: character,
+    });
+  } catch (e) {
+    console.log('error', e);
+  }
+} */
+
+class FetchCharacter extends Component {
+
+export default FetchCharacter;
