f48c813c8562942f439d549989932bb6c3da3d74
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-29 19:29:06 +1300

    Added headings for film information with props.

--- src/components/FilmInfo.js
+++ src/components/FilmInfo.js
@@ -3,5 +3,34 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const FilmInfo = ({ film }) => {
+  const { title, release_date, director, producer, episode_id } = film;
+  return (
+    <div>
+      <Hero>
+        <div>
+          <h1>{title}</h1>
+        </div>
+      </Hero>
+      <InfoWrapper>
+        <h2>
+          <b>Episode: </b>
+          {episode_id}
+        </h2>
+        <h2>
+          <b>Release Date: </b>
+          {release_date}
+        </h2>
+        <h2>
+          <b>Director: </b>
+          {director}
+        </h2>
+        <h2>
+          <b>Producers: </b>
+          {producer}
+        </h2>
+      </InfoWrapper>
+    </div>
+  );
+};
 
 export default FilmInfo;
