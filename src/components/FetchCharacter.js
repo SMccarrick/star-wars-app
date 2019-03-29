1ddd3d9f56ec598231806fce510116cbf853b626
Stephen McCarrick <smccarrick2804@gmail.com>
2019-03-30 08:34:46 +1300

    Removed un-needed comments.

--- src/components/FetchCharacter.js
+++ src/components/FetchCharacter.js
@@ -1,17 +1,4 @@
 import { Component } from 'react';
-// Fetch data from the API
-/* async componentDidMount() {
-  try {
-    const res = await fetch('https://swapi.co/api/people/');
-    const people = await res.json();
-
-    this.setState({
-      characters: character,
-    });
-  } catch (e) {
-    console.log('error', e);
-  }
-} */
 
 class FetchCharacter extends Component {
   constructor() {
