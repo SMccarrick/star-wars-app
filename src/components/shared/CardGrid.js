39327e9b65bf68a5ad1e5b5c09aff86432f4bed3
Stephen McCarrick <smccarrick2804@gmail.com>
2019-04-08 10:04:43 +1200

    Created individual components for Card component.

---
+++ src/components/shared/CardGrid.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const CardGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(3, 1fr);
+  grid-row-gap: 1rem;
+`;
+
+export default CardGrid;
