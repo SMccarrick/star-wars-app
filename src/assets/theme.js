c53b4bd52a133ca82c3c9f3b76544eaddb3ba862
Stephen McCarrick <smccarrick2804@gmail.com>
2019-04-08 10:13:16 +1200

    Added theme styles to use across app.

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -1,6 +1,15 @@
 const theme = {
-  primary: "#333333",
-  secondary: "#2D9CDB"
+  // Colours
+  primary: '#333333',
+  secondary: '#2D9CDB',
+  // Backgrounds
+  light_bg: '#f2f2f2',
+  card_bg: '#bdbdbd',
+  // Shadows
+  shadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+  // Transitions
+  transform: 'scale(1.1)',
+  transition: '0.5s',
 };
 
 export default theme;
