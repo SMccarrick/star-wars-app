e94ca7f00c8d87a449dceddad0030765ae4f1857
Stephen McCarrick <smccarrick2804@gmail.com>
2019-04-08 10:07:39 +1200

    Removed CardStyles.

--- src/components/Film/Film.js
+++ src/components/Film/Film.js
@@ -41,33 +41,3 @@ const StyledLink = styled(Link)`
     color: ${props => props.theme.primary};
   }
 `;
-
-const CardStyles = styled.div`
-  text-align: center;
-  margin: 1rem;
-  padding: 1rem;
-  background: #bdbdbd;
-  border-radius: 25px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  :hover {
-        -webkit-transform: scale(1.1);
-        -ms-transform: scale(1.1);
-        transform: scale(1.1);
-        transition: 0.5s;
-      }
-  }
-  h1 {
-    font-weight: 400;
-    font-size: 2rem;
-  }
-  p {
-    font-weight: 700;
-    font-size: 1.3rem;
-  }
-`;
-
-const CardHr = styled.hr`
-  color: ${props => props.theme.secondary};
-  border-width: 2px;
-  width: 8%;
-`;
