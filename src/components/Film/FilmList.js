9259466b775ec6d1e3e8577f6490b51780f8edd8
Stephen McCarrick <smccarrick2804@gmail.com>
2019-04-08 10:12:05 +1200

    Removed FilmGrid component.

--- src/components/Film/FilmList.js
+++ src/components/Film/FilmList.js
@@ -60,10 +60,3 @@ export default FilmList;
 const FilmListHeading = styled.h2`
   text-align: center;
 `;
-
-const FilmGrid = styled.div`
-  display: grid;
-  padding: 1rem;
-  grid-template-columns: repeat(3, 1fr);
-  grid-row-gap: 1rem;
-`;
